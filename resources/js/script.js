4591601a2677fb914eb94ab35a018686ed55f3e5
Vinay <vinay@qburst.com>
2015-06-25 16:44:43 +0530

    scoll button active

---
+++ resources/js/script.js
@@ -0,0 +1,5 @@
+$('#scroll').click(function() {
+   //optionally remove the 500 (which is time in milliseconds) of the
+   //scrolling animation to remove the animation and make it instant
+   $.scrollTo($('#first_container'), 500);
+});
