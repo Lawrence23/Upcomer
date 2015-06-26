32aeb21455d5ded58f33070ac14037e0fbf65f09
Vinay <vinay@qburst.com>
2015-06-26 17:27:19 +0530

    changes

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
-$('#scroll').click(function() {
+$('#scroll_img').click(function() {
    //optionally remove the 500 (which is time in milliseconds) of the
    //scrolling animation to remove the animation and make it instant
    $.scrollTo($('#first_container'), 500);
